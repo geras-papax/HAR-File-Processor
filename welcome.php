be960c293012da42c8a358189127be95032ccff4
Gerasimos Papachronopoulos <makis.papaxronopoulos@gmail.com>
2020-12-02 19:37:46 +0200

    UPDATED

--- welcome.php
+++ welcome.php
@@ -15,48 +15,17 @@
                <img src="Logos/exit1.png">
             </div>
          </a>
-         <form action="" method="post" enctype="multipart/form-data">
+         <div class="upload-file">
             Select file to upload:
-            <input type="file" name="fileToUpload" id="fileToUpload">
-            <input type="submit" value="Upload File" name="submit">
-         </form>
+            <input type="file" name="file" id="file">
+            <input type="button" value="Upload" id="btn_uploadfile"
+               onclick="uploadFile();">
+         </div>
       </section>
    </body>
-   
+   <script src="welcome.js"></script>
 </html>
-<?php
-$target_dir = "HarFiles/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$jsonFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if(isset($_POST["submit"])) {
-
- // Check if file already exists
- if (file_exists($target_file)) {
-   echo "Sorry, file already exists.";
-   $uploadOk = 0;
- }
- 
- // Allow certain file formats
- /*if($jsonFileType != "har") {
-   echo "Sorry, only HAR files are allowed.";
-   $uploadOk = 0;
- }*/
- 
- // Check if $uploadOk is set to 0 by an error
- if ($uploadOk == 0) {
-   echo "Sorry, your file was not uploaded.";
- // if everything is ok, try to upload file
- } else {
-   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-   } else {
-     echo "Sorry, there was an error uploading your file.";
-   }
- }
-}
-?>
 
 
 
